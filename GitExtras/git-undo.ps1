c66c7dd615379227a74d32d8897c2bea89eb8e28
Raphael Fleischlin <git@raefftec.ch>
2014-11-06 10:11:01 +0100

    added git undo command

---
+++ GitExtras/git-undo.ps1
@@ -0,0 +1,29 @@
+﻿
+[CmdletBinding(DefaultParametersetName='soft')]
+param(
+    [Parameter(ParameterSetName='hard')]
+    [switch]$hard,
+    [Parameter(ParameterSetName='soft')]
+    [switch]$soft,
+    [Parameter(ParameterSetName='hard', Position=0)]
+    [Parameter(ParameterSetName='soft', Position=0)]
+    [int]$back
+)
+
+if ($env:HOME) { (Get-PSProvider 'FileSystem').Home = $env:HOME }
+
+$resetArgument = "^"
+if ($back -gt 1) { $resetArgument = "~$back" }
+
+switch ($PSCmdlet.ParameterSetName)
+{
+    'soft'
+    {
+        git reset --soft HEAD$resetArgument
+        git reset
+    }
+    'hard'
+    {
+        git reset --hard HEAD$resetArgument
+    }
+}
