defbc192cf4da2032580d4fe653224120a7ddd4f
Raphael Fleischlin <git@raefftec.ch>
2014-11-07 01:12:13 +0100

    added create and delete branch commands

---
+++ GitExtras/git-create-branch.ps1
@@ -0,0 +1,11 @@
+﻿
+param(
+    [Parameter(Position=0, Mandatory=$true)]
+    [string]$branch
+)
+
+if ($env:HOME) { (Get-PSProvider 'FileSystem').Home = $env:HOME }
+
+git push origin HEAD:refs/heads/$branch
+git fetch origin
+git checkout --track -b $branch origin/$branch
---
+++ GitExtras/git-delete-branch.ps1
@@ -0,0 +1,23 @@
+﻿
+if ($env:HOME) { (Get-PSProvider 'FileSystem').Home = $env:HOME }
+
+if ($args.Count -eq 0)
+{
+    Write-Host "branch required!" -ForegroundColor Red
+    exit 1
+}
+
+$localBranches = ""
+$originBranches = ""
+$originRefs = ""
+
+foreach ($branch in $args)
+{
+    $localBranches += " $branch"
+    $originBranches += " origin/$branch"
+    $originRefs += " :refs/heads/$branch"
+}
+
+git branch -D $localBranches
+git branch -d -r $originBranches
+git push origin $originRefs
