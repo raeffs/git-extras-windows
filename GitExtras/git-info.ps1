fd5a161e8d1806062258bbaabf0c63e1f48a350f
Raphael Fleischlin <git@raefftec.ch>
2014-11-10 18:44:41 +0100

    added git info command

---
+++ GitExtras/git-info.ps1
@@ -0,0 +1,30 @@
+﻿
+param(
+    [switch]$noConfig
+)
+
+. init.ps1
+
+write ""
+write "## Remote URLs:"
+git remote -v
+
+write ""
+write "## Remote Branches:"
+git branch -r
+
+write ""
+write "## Local Branches:"
+git branch
+
+write ""
+write "## Most Recent Commit:"
+git log --max-count=1 --pretty=short
+write "Type 'git log' for more commits, or 'git show <commit id>' for full commit details."
+
+if (!$noConfig.IsPresent)
+{
+    write ""
+    write "## Configuration (.git/config):"
+    git config --list
+}
