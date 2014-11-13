d0c3c74fdfa3ada7665b0c93dc0a3dad01ae8dac
Raphael Fleischlin <git@raefftec.ch>
2014-11-13 19:23:59 +0100

    added git line-summary command

---
+++ GitExtras/git-line-summary.ps1
@@ -0,0 +1,22 @@
+﻿
+. init.ps1
+
+write "project : $(pwd | Split-Path -Leaf)"
+
+$lines = 0
+$linesByAuthor = @{}
+
+git grep --cached -I -l -e $'' | % {
+    git blame --line-porcelain "$_" |
+        sls "^author " |
+        % { "$_" -replace "^author ", "" } |
+        % { $lines++; $linesByAuthor["$_"]++ }
+}
+
+write "lines   : $lines"
+write "authors : "
+
+$linesByAuthor.GetEnumerator() | 
+    sort -Descending Value |
+    select Key, Value, @{Name="Percentage"; Expression={ "{0:N2} %" -f  $(100 * $_.Value / $lines) }} |
+    ft -HideTableHeaders -AutoSize -Property Value, Key, Percentage
