bbaf1cbc5ff648f2b725795f257f0e89bf6f8096
Raphael Fleischlin <git@raefftec.ch>
2014-11-16 16:31:22 +0100

    added git setup command

---
+++ GitExtras/git-setup.ps1
@@ -0,0 +1,16 @@
+﻿
+param(
+    [Parameter(Position=0)]
+    [string]$directory
+)
+
+. init.ps1
+
+if (!$directory) { $directory = "." }
+
+$(md -Force "$directory"; $?) -and
+    $(cd "$directory"; $?) -and
+    $(!(Test-Path "$directory"); $?) -and
+    $(git init; $?) -and
+    $(git add .; $?) -and
+    $(git commit -m "Initial commit"; $?) | null
