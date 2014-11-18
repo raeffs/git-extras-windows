2df3c77631f55b19ce046c5948901bc9ded6cf24
Raphael Fleischlin <git@raefftec.ch>
2014-11-18 19:00:19 +0100

    added git reset-file command

---
+++ GitExtras/git-reset-file.ps1
@@ -0,0 +1,28 @@
+﻿
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$file,
+    [Parameter(Position=1)]
+    [string]$commit
+)
+
+. init.ps1
+
+if (Test-Path "$file" -PathType Leaf)
+{
+    git rm --cached -q -f -- "$file"
+    if ($commit)
+    {
+        git checkout "$commit" -- "$file"
+        write "Reset '$file' to $commit"
+    }
+    else
+    {
+        git checkout HEAD -- "$file"
+        write "Reset '$file' to HEAD"
+    }
+}
+else
+{
+    write "File '$file' not found in '$(pwd)'"
+}
