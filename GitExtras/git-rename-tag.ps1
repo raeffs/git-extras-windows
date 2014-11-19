010337d9bd54370b2e50d5cf4a4bba7b7393194e
Raphael Fleischlin <git@raefftec.ch>
2014-11-19 18:24:35 +0100

    added git rename-tag command

---
+++ GitExtras/git-rename-tag.ps1
@@ -0,0 +1,14 @@
+﻿
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$old,
+    [Parameter(Mandatory=$true, Position=1)]
+    [string]$new
+)
+
+. init.ps1
+
+git tag "$new" "$old"
+git tag -d "$old"
+git push origin "$new"
+git push origin ":refs/tags/$old"
