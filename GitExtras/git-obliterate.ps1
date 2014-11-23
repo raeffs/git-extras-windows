fcd491815ca44ac4604b5b1578d8af49f756fc22
Raphael Fleischlin <git@raefftec.ch>
2014-11-23 13:30:56 +0100

    added git obliterate command

---
+++ GitExtras/git-obliterate.ps1
@@ -0,0 +1,9 @@
+﻿
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$file
+)
+
+. init.ps1
+
+git filter-branch -f --index-filter "git rm -r --cached '$file' --ignore-unmatch" --prune-empty --tag-name-filter cat -- --all
