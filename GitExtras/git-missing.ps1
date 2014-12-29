317bee6e12fc90a631028baa912f63ea9807e131
Raphael Fleischlin <git@raefftec.ch>
2014-12-29 19:01:29 +0100

    added git missing command

---
+++ GitExtras/git-missing.ps1
@@ -0,0 +1,24 @@
+﻿
+. init.ps1
+
+$args | % {
+    if (sls "^-.+" -InputObject $_ -Quiet -NotMatch)
+    {
+        if (!$firstBranch) { $firstBranch = "$_"; return }
+        if (!$secondBranch) { $secondBranch = "$_"; return }
+    }
+    else
+    {
+        $passthrough = "$passthrough $_"
+    }
+}
+
+if (!$firstBranch) { error "At least one branch is required!" }
+
+if (!$secondBranch)
+{
+    $secondBranch = $firstBranch
+    $firstBranch = ""
+}
+
+git log $passthrough.Trim() $firstBranch...$secondBranch --format="%m %h %s" --left-right
