1ef0d3acdfe25ed6e70fb484b138a5d3f4851d74
Raphael Fleischlin <git@raefftec.ch>
2014-12-29 18:53:55 +0100

    added git graft command

---
+++ GitExtras/git-graft.ps1
@@ -0,0 +1,13 @@
+﻿
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$source,
+    [Parameter(Mandatory=$false, Position=1)]
+    [string]$destination
+)
+
+. init.ps1
+
+$(git checkout $destination; $?) -and
+    $(git merge --no-ff $source; $?) -and
+    $(git branch -d $source; $?) | null
