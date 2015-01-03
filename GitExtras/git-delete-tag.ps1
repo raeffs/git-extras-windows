ef9382b8befb55d03944370610fa31f6897a509a
Raphael Fleischlin <git@raefftec.ch>
2015-01-03 18:59:18 +0100

    added git delete-tag command

---
+++ GitExtras/git-delete-tag.ps1
@@ -0,0 +1,34 @@
+﻿
+. init.ps1
+
+function git-delete-tag
+{
+    <#
+
+    .SYNOPSIS
+    Delete tags
+
+    .DESCRIPTION
+    Deletes local and remote tags.
+
+    .PARAMETER tags
+    The name of the tag to delete. If multiple tags are provided, then they will all be deleted.
+
+    #>
+
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [string[]]$tags
+    )
+
+    $tags | 
+        % { 
+            $localTags = "$localTags $_"
+            $originRefs = "$originRefs :refs/tags/$_"
+        }
+
+    git tag -d $localTags.Trim()
+    git push origin $originRefs.Trim()
+}
+
+git-delete-tag @args
