31fb8bfe103cfb4a72b4a3bee5a020cdf127b9e2
Raphael Fleischlin <git@raefftec.ch>
2015-03-18 20:51:12 +0100

    when deleting merged branches, preserve 'svn' branch for git-svn

--- GitExtras/git-delete-merged-branches.ps1
+++ GitExtras/git-delete-merged-branches.ps1
@@ -1,4 +1,4 @@
 ﻿
 . init.ps1
 
-git branch --no-color --merged | sls -NotMatch "\*" | sls -NotMatch "master" | % { git branch -d "$_".Trim() }
+git branch --no-color --merged | sls -NotMatch "\*" | sls -NotMatch "master" | sls -NotMatch "svn" | % { git branch -d "$_".Trim() }
